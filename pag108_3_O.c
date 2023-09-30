6ff6c8840e333ffeccd6cc300119eddc8ff7b703
jasonxb09 <142895801+jasonxb09@users.noreply.github.com>
2023-09-30 16:22:01 -0300

    Add files via upload

---
+++ pag108_3_O.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a, r;
+    printf("digite o valor a ser mltiplicado: ");
+    scanf("%i", &1a);
+    r=a*2;
+    if(r>30){
+        printf("a multiplicacao resulta em: %i", r);
+    }
+}
