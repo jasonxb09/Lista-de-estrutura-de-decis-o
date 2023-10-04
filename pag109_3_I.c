bcf8c6fe8dd4ba530e06b51211ef5ec324be64ca
jasonxb09 <142895801+jasonxb09@users.noreply.github.com>
2023-10-03 22:28:28 -0300

    Add files via upload

---
+++ pag109_3_I.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+float main()
+{
+    int a,b,c,d,e,maior,menor;
+    printf("escreva o primeiro valor: ");
+    scanf("%i",&a);
+    printf("escreva o segundo valor: ");
+    scanf("%i",&b);
+    printf("escreva o terceiro valor: ");
+    scanf("%i",&c);
+    printf("escreva o quarto valor: ");
+    scanf("%i",&d);
+    printf("escreva o quinto valor: ");
+    scanf("%i",&e);
+    maior=a;
+    menor=a;
+    if(b>maior){
+        maior=b;
+    }
+    else if (b<menor){
+        menor=b;
+    }
+    if(c>maior){
+        maior=c;
+    }
+    else if (c<menor){
+        menor=c;
+    }
+    if(d>maior){
+        maior=d;
+    }
+    else if (d<menor){
+        menor=d;
+    }
+    if(e>maior){
+        maior=e;
+    }
+    else if (e<menor){
+        menor=e;
+    }
+    printf("maior valor: %i\n",maior);
+    printf("menor valor: %i",menor);
+}
