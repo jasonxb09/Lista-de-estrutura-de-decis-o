4a222c65ff07607e5df8f08f7d0d0d618a1cf279
jasonxb09 <142895801+jasonxb09@users.noreply.github.com>
2023-10-03 22:38:36 -0300

    Add files via upload

---
+++ pag109_3_L.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("digite um numero para verificar se ele e menor que 3: ");
+    scanf("%i",&n);
+    if (!(n>3)){
+        printf("%i",n);
+    }
+    else{
+        printf("e maior que 3");
+    }
+}
