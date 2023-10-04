5185cffe74c8b365f38a01673dfa5de2707cb9da
jasonxb09 <142895801+jasonxb09@users.noreply.github.com>
2023-10-03 22:47:17 -0300

    Add files via upload

---
+++ pag143_36.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+float main()
+{
+     float c,e,t;
+     printf("valor da compra: ");
+     scanf("%f",&c);
+     printf("valor entregue:");
+     scanf("%f", &e);
+     t=e-c;
+     if(t>=0){
+        printf("valor suficiente\n");
+        printf("troco a ser devolvido: RS%.2f", t);
+     }
+     else{
+        printf("valor insuficiente");
+     }
+}
