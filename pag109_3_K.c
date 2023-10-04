600f8914bb358cf0deb2a42a6fccdcc6ade35cea
jasonxb09 <142895801+jasonxb09@users.noreply.github.com>
2023-10-03 22:33:10 -0300

    Add files via upload

---
+++ pag109_3_K.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("escreva o valor para saber se ele esta entre 1 e 9: ");
+    scanf("%i",&n);
+    if((n>=1)&&(n<=9)){
+        printf("esta na faixa permitida");
+    }
+    else{
+        printf("nao esta na faixa permitida");
+    }
+}
