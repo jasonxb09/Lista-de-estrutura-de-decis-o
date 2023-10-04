d17c0cf3d8b8293e2d0b0745b1e19bc9b313ee26
jasonxb09 <142895801+jasonxb09@users.noreply.github.com>
2023-10-03 22:14:54 -0300

    Add files via upload

---
+++ pag109_3_G.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a, b, c, d;
+    scanf("%i %i %i %i",&a,&b,&c,&d);
+    if((a%2==0)&&(a%3==0)){
+        printf("%i ",a);
+    }
+    if((b%2==0)&&(b%3==0)){
+        printf("%i",b);
+    }
+    if((c%2==0)&&(c%3==0)){
+        printf(" %i ",c);
+    }
+    if((d%2==0)&&(d%3==0)){
+        printf("%i",d);
+    }
+}
