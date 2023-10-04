591d3e3569b0c1ef9e04e8509c953e7b783ff3f4
jasonxb09 <142895801+jasonxb09@users.noreply.github.com>
2023-10-03 22:30:53 -0300

    Add files via upload

---
+++ pag109_3_J.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n;
+    printf("escreva o nmero que deseja saber se e par ou impar4: ");
+    scanf("%i",&n);
+    if(n%2==0){
+        printf("o numero e par");
+    }
+    else
+    {
+        printf("o numero e impar");
+    }
+}
